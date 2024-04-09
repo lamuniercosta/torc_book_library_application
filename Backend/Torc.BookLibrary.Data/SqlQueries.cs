844db685c1be15245d4abedee0e7e95918798b8b
lamuniercosta <lamunier.costa@gmail.com>
2024-04-09 00:40:26 -0300

    First commit

---
+++ Backend/Torc.BookLibrary.Data/SqlQueries.cs
@@ -0,0 +1,17 @@
+﻿namespace Torc.BookLibrary.Data;
+
+public static class SqlQueries
+{
+    public const string GetBooks = """
+                                   SELECT
+                                   Title,
+                                   Publisher,
+                                   CONCAT(FirstName, " ", LastName) AS Authors,
+                                   Type,
+                                   ISBN,
+                                   Category,
+                                   CONCAT(TotalCopies - CopiesInUse, '/', TotalCopies) AS AvailableCopies
+                                   FROM BOOKS
+                                   /**where**/
+                                   """;
+}
