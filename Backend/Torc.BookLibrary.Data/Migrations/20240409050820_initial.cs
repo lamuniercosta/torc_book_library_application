e87491af775c13c77c55978e0eec7e7de95abf71
lamuniercosta <lamunier.costa@gmail.com>
2024-04-09 02:09:17 -0300

    Adding migrations

---
+++ Backend/Torc.BookLibrary.Data/Migrations/20240409050820_initial.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Torc.BookLibrary.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    FirstName = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    TotalCopies = table.Column<int>(type: "int", nullable: false, defaultValue: 0),
+                    CopiesInUse = table.Column<int>(type: "int", nullable: false, defaultValue: 0),
+                    Type = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    ISBN = table.Column<string>(type: "varchar(80)", maxLength: 80, nullable: false),
+                    Category = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.BookId);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_Category",
+                table: "Books",
+                column: "Category");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_FirstName",
+                table: "Books",
+                column: "FirstName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_ISBN",
+                table: "Books",
+                column: "ISBN");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_LastName",
+                table: "Books",
+                column: "LastName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_Title",
+                table: "Books",
+                column: "Title");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_Type",
+                table: "Books",
+                column: "Type");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
