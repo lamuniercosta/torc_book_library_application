d6389072a0a9b7b89ecb6ffd63a29017d4208a6d
lamuniercosta <lamunier.costa@gmail.com>
2024-04-09 01:59:43 -0300

    Fixing issues with mysql

--- Backend/Torc.BookLibrary.Configuration/DependencyInjectionService.cs
+++ Backend/Torc.BookLibrary.Configuration/DependencyInjectionService.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Torc.BookLibrary.Business;
 using Torc.BookLibrary.Data;
 
 namespace Torc.BookLibrary.Configuration;
@@ -9,7 +9,8 @@ public class DependencyInjectionService
 {
     public static void Configure(IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<LibraryDbContext>(options =>
-            options.UseMySQL(configuration.GetConnectionString("DefaultConnection")));
+        services.AddSingleton(typeof(LibraryDbContext));
+        services.AddTransient(typeof(MigrationService));
+        services.AddTransient<IBookService, BookService>();
     }
 }
